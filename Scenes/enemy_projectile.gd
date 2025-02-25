69a10ece9780ceab2b847e0fac705b64431ccf5f
Yanka Sikder <ys4780@nyu.edu>
2025-02-24 23:43:05 -0500

    resetting projectile stuff for player

--- Scenes/enemy_projectile.gd
+++ Scenes/enemy_projectile.gd
@@ -1,24 +1,35 @@
 extends Area2D
+#class_name Projectile
 
 @onready var sprite: AnimatedSprite2D = $Sprite
 
-var speed: float = 300
-var direction: Vector2 = Vector2.RIGHT
-var damage: int = 1
+var speed = 400
+var direction = Vector2.RIGHT
+var damage = 1
 var source: String = "Enemy"
 
-func _ready():
+func _ready() -> void:
 	sprite.play("default")
+	rotate_sprite()
 	connect("body_entered", Callable(self, "_on_body_entered"))
-	$VisibleOnScreenNotifier2D.connect("screen_exited", Callable(self, "_on_screen_exited"))
 
 func _physics_process(delta):
-	position += direction * speed * delta
+	if direction != Vector2.ZERO:
+		position += direction * speed * delta
 
-func _on_body_entered(body: Node2D):
+func rotate_sprite():
+	var angle = direction.angle()
+	sprite.rotation = angle
+
+# SIGNALS
+
+func _on_body_entered(body: Node2D) -> void:
 	if source == "Enemy" and body is Player:
+		return
+	
+	if body.has_method("take_damage"):
 		body.take_damage(damage)
-		queue_free()
-
-func _on_screen_exited():
 	queue_free()
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free() 
